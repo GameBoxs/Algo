d9f25efc74fe8ba2ff803a47605a48ee81281405
JihunKim <72652986+GameBoxs@users.noreply.github.com>
2023-04-18 10:34:10 +0900

    Update 숫자의 표현.cpp
    주석 추가

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.cpp
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -4,7 +4,10 @@
 using namespace std;
 
 int solution(int n) {
+    // 초기 자기 자신을 포함하기 때문에 answer는 1부터 시작
     int answer = 1;
+    // n나누기 2의 값을 초과한 숫자들을 연속으로 더해서는 절대 n의 값이 나오지 않음.
+    // 시간을 줄이기 위해 n/2이하의 값만 돌도록 했음.
     for(int i=1; i<=n/2; i++) {
         int temp = i;
         for(int j=i+1; j<=n; j++) {
